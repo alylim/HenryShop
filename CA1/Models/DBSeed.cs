0b754769adfce1f4333501bca1ac8e0f66c79386
alylim <lim.alywx@gmail.com>
2022-06-27 11:30:53 +0800

    Revert "SEEED"
    This reverts commit d16c7640e0a01a633a184925f9a7c8b1c1aa3ca9.

--- CA1/Models/DBSeed.cs
+++ CA1/Models/DBSeed.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Security.Cryptography;
+﻿using System.Security.Cryptography;
 using System.Text;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-
 
 namespace CA1.Models
 {
@@ -21,8 +16,7 @@ namespace CA1.Models
         {
             SeedProduct();
             SeedUser();
-            SeedInventory();
-            
+            dbContext.SaveChanges();
 
         }
 
@@ -33,64 +27,47 @@ namespace CA1.Models
             dbContext.Add(new Product
             {
 
-                Name = ".NET Charts",
-                Desc = "Brings powerful charting capabilities to your .NET applications",
+                Name = ".NET charts",
+                Desc = "blah blah blah",
                 Img = "./img/chart.png",
-                Price = 99,
-                DownLoadLink = "www.12345.com"
+                Price = 12
             });
             
             dbContext.Add(new Product
             {
 
-                Name = ".NET PayPal",
-                Desc = "Integrate your .NET apps with PayPal the easy way!",
+                Name = ".NET Tops",
+                Desc = "blah blah blah",
                 Img = "./img/top.png",
-                Price = 69,
-                DownLoadLink = "www.23456.com"
+                Price = 123
             });
 
             dbContext.Add(new Product
             {
 
-                Name = ".NET ML",
-                Desc = "Supercharged .NET machine learning libraries",
+                Name = ".NET beds",
+                Desc = "blah blah blah",
                 Img = "./img/bed.png",
-                Price = 299,
-                DownLoadLink = "www.34567.com"
+                Price = 1234
             });
 
             dbContext.Add(new Product
             {
 
-                Name = ".NET Analytics",
-                Desc = "Performs data mining and analytics easily in .NET",
+                Name = ".NET chart2",
+                Desc = "blah blah blah",
                 Img = "./img/chart.png",
-                Price = 299,
-                DownLoadLink = "45678.com"
+                Price = 12345
             });
 
             dbContext.Add(new Product
             {
 
-                Name = ".NET Logger",
-                Desc = "Logs and aggregates events easily in your .NET apps",
+                Name = ".NET tops2",
+                Desc = "blah blah blah",
                 Img = "./img/top.png",
-                Price = 49,
-                DownLoadLink = "56789.com"
+                Price = 12346
             });
-
-            dbContext.Add(new Product
-            {
-
-                Name = ".NET Numerics",
-                Desc = "Powerful numerical methods for your .NET simulations",
-                Img = "./img/top.png",
-                Price = 199,
-                DownLoadLink = "56789.com"
-            });
-
-            dbContext.SaveChanges();
         }
 
         public void SeedUser()
@@ -98,135 +75,21 @@ namespace CA1.Models
             // get a hash algorithm object
             HashAlgorithm sha = SHA256.Create();
 
-           string username = "user1";
+            string username = "user1";
             string password = "password";
             string combo = username + password;
             byte[] hash = sha.ComputeHash(Encoding.UTF8.GetBytes(combo));
-
-            ShopCart cart = new ShopCart();
+           
             dbContext.Add(new User
             {
                 UserName = username,
-                PassHash = hash,
-                shopcart = cart
+                PassHash = hash
             });
 
-            string username2 = "user2";
-            string password2 = "password2";
-            string combo2 = username2 + password2;
-            byte[] hash2 = sha.ComputeHash(Encoding.UTF8.GetBytes(combo2));
-
-            ShopCart cart2 = new ShopCart();
-            dbContext.Add(new User
-            {
-                UserName = username2,
-                PassHash = hash2,
-                shopcart = cart2
-            });
-
-            dbContext.SaveChanges();
-
-        }
-
-        public void SeedInventory()
-        {
-            //id, activation code, and product id
-
-            Product product = dbContext.Products.FirstOrDefault(x=> x.Name == ".NET Charts");
-            if (product != null)
-            {
-                for (int i = 0; i < 10; i++)
-                {
-                    InventoryRecord record = new InventoryRecord
-                    {
-                        ActivationId = Guid.NewGuid() //using static method to call instead of default constructor because if not the GUID reverts to default all 0s.
-                    };
-
-                    product.InventoryRecords.Add(record);
-
-                }
-            }
-
-            Product product2 = dbContext.Products.FirstOrDefault(x => x.Name == ".NET PayPal");
-            if (product != null)
-            {
-                for (int i = 0; i < 5; i++)
-                {
-                    InventoryRecord record2 = new InventoryRecord
-                    {
-                        ActivationId = Guid.NewGuid() //using static method to call instead of default constructor because if not the GUID reverts to default all 0s.
-                    };
-
-                    product2.InventoryRecords.Add(record2);
-
-                }
-            }
-
-            Product product3 = dbContext.Products.FirstOrDefault(x => x.Name == "ML");
-            if (product3 != null)
-            {
-                for (int i = 0; i < 1; i++)
-                {
-                    InventoryRecord record3 = new InventoryRecord
-                    {
-                        ActivationId = Guid.NewGuid() //using static method to call instead of default constructor because if not the GUID reverts to default all 0s.
-                    };
-
-                    product3.InventoryRecords.Add(record3);
-
-                }
-            }
-
-            Product product4 = dbContext.Products.FirstOrDefault(x => x.Name == ".NET Analytics");
-            if (product4 != null)
-            {
-                for (int i = 0; i < 2; i++)
-                {
-                    InventoryRecord record4 = new InventoryRecord
-                    {
-                        ActivationId = Guid.NewGuid() //using static method to call instead of default constructor because if not the GUID reverts to default all 0s.
-                    };
-
-                    product4.InventoryRecords.Add(record4);
-
-                }
-            }
-
-            Product product5 = dbContext.Products.FirstOrDefault(x => x.Name == ".NET Logger");
-            if (product5 != null)
-            {
-                for (int i = 0; i < 10; i++)
-                {
-                    InventoryRecord record5 = new InventoryRecord
-                    {
-                        ActivationId = Guid.NewGuid() //using static method to call instead of default constructor because if not the GUID reverts to default all 0s.
-                    };
-
-                    product5.InventoryRecords.Add(record5);
-
-                }
-            }
-
-            Product product6 = dbContext.Products.FirstOrDefault(x => x.Name == ".NET Numerics");
-            if (product6 != null)
-            {
-                for (int i = 0; i < 4; i++)
-                {
-                    InventoryRecord record6 = new InventoryRecord
-                    {
-                        ActivationId = Guid.NewGuid() //using static method to call instead of default constructor because if not the GUID reverts to default all 0s.
-                    };
-
-                    product6.InventoryRecords.Add(record6);
-
-                }
-            }
-            dbContext.SaveChanges();
-        }
 
 
         
-    }
+        }
 
-    
+    }
 }
